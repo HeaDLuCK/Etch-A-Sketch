0b766f34df64764e3cf539213ec69f4313885d62
HeaDLuCK <106028299+HeaDLuCK@users.noreply.github.com>
2022-12-23 21:43:52 +0100

    Add  three buttons and  setup events , slice code to function
    first of all we add three button to specific the  with wthat you wanna
    play ranbow or faded or black
    after that we fix the code by slice it to function and merge them

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,27 @@
+let buttonsContainer = document.createElement('div')
+buttonsContainer.classList.add('btnContainer')
+
+let black = document.createElement('button')
+let rainbow = document.createElement('button')
+let faded = document.createElement('button')
+
+black.setAttribute('class', 'function')
+rainbow.setAttribute('class', 'function')
+faded.setAttribute('class', 'function')
+
+black.textContent = 'Black'
+rainbow.textContent = 'rainbow'
+faded.textContent = 'faded'
+
+buttonsContainer.append(black, rainbow, faded)
+document.body.append(buttonsContainer);
+
+
+let confirmGrids = 16
+let whichGame = 1
+
+
+
 function blackBlock(elm) {
     return elm.style.backgroundColor = 'black'
 }
@@ -16,51 +40,88 @@ function fadedBlackBlock(elm, fade) {
     }
 }
 
+function gridGame(confirmGrids, test) {
+    let container = document.createElement('div');
+    container.classList.add('container');
+
+    for (let i = 1; i <= confirmGrids; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 1; j <= confirmGrids; j++) {
+            let col = document.createElement('div');
+            col.classList.add('col');
+            let count = 0
+            switch (test) {
+
+                // black Blocks here 
+
+                case 1:
+
+
+                    col.addEventListener('mouseover', () => {
+                        blackBlock(col);
+                    })
+                    break;
+
+                // rainbow Blocks here 
+
+                case 2:
+                    col.addEventListener('mouseover', () => {
+                        rainbowBlock(col);
+                    })
+                    break;
+
+                // faded Blocks here 
+
+                case 3:
+
+                    col.addEventListener('mouseover', () => {
+
+                        fadedBlackBlock(col, count)
+                        count += 10
+                    })
+                    break;
+            }
+
+            row.appendChild(col);
+
+        };
+        container.appendChild(row);
+    };
+    button.after(container);
+};
+
+function newGame() {
+    checkDiv = document.querySelector('.container')
+    if (checkDiv) {
+        checkDiv.remove()
+        gridGame(confirmGrids, whichGame)
+    };
+}
+
+
+black.onclick = () => {
+    whichGame = 1;
+    newGame()
+};
+rainbow.onclick = () => {
+    whichGame = 2
+    newGame()
+};
+faded.onclick = () => {
+    whichGame = 3
+    newGame()
+};
 
 
 let button = document.createElement('button')
 button.setAttribute("id", "btn");
 button.textContent = 'Click to  choose how many grids you want :D'
 document.body.prepend(button);
-let confirmGrids = 16
-let container = document.createElement('div');
-container.classList.add('container');
-
-for (let i = 1; i <= confirmGrids; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 1; j <= confirmGrids; j++) {
-        let col = document.createElement('div');
-        col.classList.add('col');
-        // black Blocks here 
-        
-        // col.addEventListener('mouseover', () => {
-        //     blackBlock(col);
-        // })
 
 
-        // rainbow Blocks here 
 
-        // col.addEventListener('mouseover', () => {
-        //     rainbowBlock(col);
-        // })
-
-
-        // faded Blocks here 
-
-        // let count = 0
-        // col.addEventListener('mouseover', () => {
-            
-        //     fadedBlackBlock(col, count)
-        //     count+=10
-        // })
-
-        row.appendChild(col);
-
-    };
-    container.appendChild(row);
-};
-button.after(container);
+gridGame(confirmGrids, whichGame)
 
 button.addEventListener('click', () => {
 
@@ -74,51 +135,7 @@ button.addEventListener('click', () => {
         };
 
     } while (check);
-    checkDiv = document.querySelector('.container')
-    if (checkDiv) {
-        checkDiv.remove()
-        let container = document.createElement('div');
-        container.classList.add('container');
-
-        for (let i = 1; i <= confirmGrids; i++) {
-            let row = document.createElement('div');
-            row.classList.add('row');
-            for (let j = 1; j <= confirmGrids; j++) {
-                let col = document.createElement('div');
-                col.classList.add('col');
-
-                // black Blocks here 
-                
-                // col.addEventListener('mouseover', () => {
-                //     blackBlock(col);
-                // })
-
-
-                // rainbow Blocks here 
-
-
-                // col.addEventListener('mouseover', () => {
-                //     rainbowBlock(col);
-                // })
-
-                // fadedd Blocks here 
-                // let count = 0
-                // col.addEventListener('mouseover', () => {
-
-                //     fadedBlackBlock(col, count)
-                //     count += 10
-                // })
-                row.appendChild(col);
-
-            };
-            container.appendChild(row);
-        };
-        button.after(container);
-    };
-
-
-
-
+    newGame()
 })
 
 // console.log(confirmGrids);
